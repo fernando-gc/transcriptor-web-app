78e8cd550a1191d3d3ce81f4a3f54b1eeaa5934c
Laboratorio Digital <fer.grac@hotmail.com>
2025-07-22 12:07:05 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+
+from flask import Flask, request, render_template, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'wav', 'mp3', 'm4a'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'audiofile' not in request.files:
+            return 'No file part'
+        file = request.files['audiofile']
+        if file.filename == '':
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            return f'Archivo {filename} subido correctamente.'
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    app.run(debug=True)
